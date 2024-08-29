3dbb6fcfac5c42a781eb7142f34bc3304500aaa1
Doad Software Department <doad.software@gmail.com>
2024-08-29 11:10:37 +0530

    add scroll in website

--- src/components/About.js
+++ src/components/About.js
@@ -10,37 +10,27 @@ const AboutSection = () => {
         <div className="text-center">
           <h2 className="text-3xl font-bold mb-4">ABOUT US</h2>
           <p className="text-lg mb-6 pl-44 pr-44 uppercase">
-            We are basically a bunch of sports fans & lovers who eventually ended up making their careers in 
-            the visual and sports broadcast industry. With a cumulative experience of more than 30 years, 
-            we have passion, experience & industry know.
+            We are a team of sports enthusiasts who have turned our passion into successful careers in the visual and sports broadcasting industry.
+            With a combined experience of over 30 years, we bring a deep passion, extensive experience, and industry expertise to every project.
           </p>
           <p className="text-lg pl-44 pr-44 uppercase">
-            Design on a Dime was incorporated as a private limit company registered under the Companies Act of India, 2010.
+            Design on a Dime was established as a private limited company, registered under the Companies Act of India, 2010.
           </p>
           <p className="text-lg mb-6 pl-44 pr-44 uppercase">
-            Our aim is simple, organic and was naturally birthed owing to us being both sports & entertainment afficionados 
-            as well as passionate end consumers. To provide cost effective & end to end solutions for all the graphics needs 
-            of our clients on a global scale by using the most up to date technology and in house innovations available. 
-            Our previous organizations have seen us as project leads and we have delivered solutions for premier sporting events 
-            like the Indian Premier League, Big Bash League, Caribbean Premier League just to name a few.
+            Our mission is straightforward and organically born from our love for sports, entertainment, and the consumer experience. 
+            We aim to provide cost-effective, end-to-end solutions for all our clients' graphic needs on a global scale, utilizing the 
+            latest technology and in-house innovations. In our previous roles, we have led projects and delivered solutions for premier 
+            sporting events such as the Indian Premier League, Big Bash League, and Caribbean Premier League, among others.
           </p>
           <p className="text-lg pl-44 pr-44 uppercase">
-            We have also worked closely with the television broadcast houses like Sony, Star, Ten Sports, and Zee.
+            We have also collaborated closely with leading television broadcast networks like Sony, Star, Ten Sports, and Zee, 
+            delivering a variety of graphics packages across their channels. Additionally, we have played a key role in the creation 
+            of intellectual properties, most notably the Pro Kabaddi League, where we conceptualized the entire show. Our goal was 
+            to engage viewers with cutting-edge video graphics and innovative sports data, revolutionizing the sport for everyone.
           </p>
           <p className="text-lg mb-6 pl-44 pr-44 uppercase">
-            We helped them in delivering various graphics packages for through their channels.
-          </p>
-          <p className="text-lg pl-44 pr-44 uppercase">
-            We have also helped them in creating & intellectual properties like the famous Pro Kabaddi League.
-          </p>
-          <p className="text-lg mb-6 pl-44 pr-44 uppercase">
-            We conceptualized the entire show and our aim was to engage the viewers with cutting edge video graphics 
-            and new sports data to basically revolutionize the sport for all. We are not only limited to 
-            Visual Graphics Broadcasting but we also cater to the world of Virtual Reality.
-          </p>
-          <p className="text-lg pl-44 pr-44 uppercase">
-            We also specialize in delivering the news room solutions virtually, in terms of Design and/or with 
-            data integration input.
+            Our expertise extends beyond visual graphics broadcasting; we also excel in the realm of Virtual Reality. 
+            Moreover, we specialize in delivering virtual newsroom solutions, encompassing both design and data integration.
           </p>
         </div>
       </div>
