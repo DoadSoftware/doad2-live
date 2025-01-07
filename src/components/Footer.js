db553f6f00d20b9f0bb68cf791f943e239cc8f0b
Doad Software Department <98625971+DoadSoftware@users.noreply.github.com>
2025-01-07 14:14:34 +0530

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,8 @@ const Footer = () => (
     <div className="container mx-auto text-center">
       <p>© 2024 Design on a Dime Pvt. Ltd. All rights reserved.</p>
       <div className="flex justify-center space-x-4 mt-2">
-        <a href="https://www.youtube.com/channel/UClnasVkidk9vqkObszisnfg" target="_blank"rel="noopener noreferrer"className="fa-brands fa-youtube"style={{ color: '#FF0000', fontSize: '1.6rem' }} />
-        <a href="https://www.linkedin.com/company/doadlive/"  target="_blank" rel="noopener noreferrer" className="fa-brands fa-linkedin"   style={{ color: '#0077B5', fontSize: '1.6rem' }} />
+        <a href="https://www.youtube.com/channel/UClnasVkidk9vqkObszisnfg" target="_blank"rel="noopener noreferrer"className="fa-brands fa-youtube"style={{ color: '#FF0000', fontSize: '1.6rem' }}></a>
+        <a href="https://www.linkedin.com/company/doadlive/"  target="_blank" rel="noopener noreferrer" className="fa-brands fa-linkedin"   style={{ color: '#0077B5', fontSize: '1.6rem' }}></a>
       </div>
     </div>
   </footer>
